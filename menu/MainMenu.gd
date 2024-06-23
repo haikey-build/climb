096ce9ffbc590d7abf428947e16b6cc2b64aa401
nicholas <nicholas.j.brooking@gmail.com>
2024-06-23 19:58:31 +1000

    add main menu

---
+++ menu/MainMenu.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready var level_1 = preload("res://level/proto_level.tscn")
+@onready var camera = $CameraMenu
+
+
+func _ready():
+	camera.enabled = true
+	camera.make_current()
+
+
+func _on_button_play_button_down():
+	get_tree().change_scene_to_packed(level_1)
+
+
+func _on_button_quit_button_down():
+	get_tree().quit()
